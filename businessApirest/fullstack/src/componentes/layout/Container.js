9ddbdaa87db6c3a90bb771c2586bb1bc85cefbf7
Célio Da Silva <celio01t@gmail.com>
2023-07-20 14:34:36 -0300

    feat : new cod

--- businessApirest/fullstack/src/componentes/layout/Container.js
+++
@@ -1,5 +0,0 @@
-import style from "../layout/Container.module.css";
-
-export default function Container({ children }) {
-  return <div className={style.container}>{children}</div>;
-}
---
+++ fullstack business/frontend/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+// 2 reaproveitamento de estrutura
+import { Outlet } from "react-router-dom";
+
+// 4 - navegando entre paginas
+import NavBar from "./componentes/layout/NavBar";
+import Footer from "./componentes/layout/Footer";
+
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <Outlet />
+      <ToastContainer pauseOnHover={false} autoClose={3000} position={toast.POSITION.BOTTOM_RIGHT_RIGHT} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
